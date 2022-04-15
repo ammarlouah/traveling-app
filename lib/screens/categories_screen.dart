96f262520f011acad298f87f07bc89edc62f4e38
Ammar <ammarlouah9@gmail.com>
2022-04-15 15:07:59 +0000

    First step on Categories screen we create folders and files.dart and put some constants

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  //const CategoriesScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('دليل سياحي'),
+      ),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 7 / 8,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+        ),
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
